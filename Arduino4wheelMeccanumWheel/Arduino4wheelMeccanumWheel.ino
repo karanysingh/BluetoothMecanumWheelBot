e04b8bc29c6bb0f1fc99bef33c3ffa9cd22a264d
Karan Yuvraj Singh <56989043+karanysingh@users.noreply.github.com>
2020-10-16 18:32:51 +0530

    Some conditions updated

--- Arduino4wheelMeccanumWheel/Arduino4wheelMeccanumWheel.ino
+++ Arduino4wheelMeccanumWheel/Arduino4wheelMeccanumWheel.ino
@@ -1,20 +1,22 @@
-const int trigPin_0= 2,trigPin_90=8,echoPin_90=7;
-const int echoPin_0 = 5;
+#include<softwareSerial.h>
+const int rx_pin;
+const int tx_pin;
 
-int motor1_IN1=11;
-int motor1_IN2=12;
+SoftwareSerial blue_mod(rx_pin,tx_pin);
 
-int motor2_IN3=9;
-int motor2_IN4=10;
+int d_front_l=11;
+int d_front_r=12;
+int p_front_l=11;
+int p_front_r=12;
 
-int motor3_IN5=13;
-int motor3_IN6=14;
-
-int motor4_IN7=15;
-int motor4_IN8=16;
+int d_back_l=11;
+int d_back_r=12;
+int p_back_l=11;
+int p_back_r=12;
 
 char state;
 char mode;
+
 void setup() {
 pinMode(trigPin_0, OUTPUT); 
 pinMode(echoPin_0, INPUT); 
@@ -26,142 +28,144 @@ pinMode(motor2_IN3,OUTPUT);
 pinMode(motor2_IN4,OUTPUT);
 
 extern char state; // global variable
-state = 'P';//No movement initially
+state = '-';//No movement initially
 Serial.begin(9600); 
 }
 //The code inside loop keeps on looping forever.
 void loop(){
   // Reads the data from the serial port (i.e from bluetooth in our case)
   // It receives one of the characters from W A S D L R.
-   state = Serial.read(); 
-
+  if(Serial.available())
+  {
+   state = Serial.read();
+   execution(state); 
+  }
+  else
+  {
+    execution("-");
+  }
   // execution(state) function takes in received character as argument.
-   execution(state);
+   
 }
 
 //Method responsible for controlling motors
 //
+
+
+void forward()
+{
+  //all wheels forward
+  digitalWrite(d_front_l,HIGH);
+  digitalWrite(p_front_l,HIGH);
+  digitalWrite(d_front_r,HIGH);
+  digitalWrite(p_front_r,HIGH);
+  digitalWrite(d_back_l,HIGH);
+  digitalWrite(p_back_l,HIGH);
+  digitalWrite(d_back_r,HIGH);
+  digitalWrite(p_back_r,HIGH);
+}
+
+void backward()
+{
+  digitalWrite(d_front_l,LOW);
+  digitalWrite(p_front_l,HIGH);
+  digitalWrite(d_front_r,LOW);
+  digitalWrite(p_front_r,HIGH);
+  digitalWrite(d_back_l,LOW);
+  digitalWrite(p_back_l,HIGH);
+  digitalWrite(d_back_r,LOW);
+  digitalWrite(p_back_r,HIGH);
+}
+
+void left()
+{
+  digitalWrite(d_front_l,HIGH);
+  digitalWrite(p_front_l,HIGH);
+  digitalWrite(d_front_r,HIGH;
+  digitalWrite(p_front_r,HIGH);
+  digitalWrite(d_back_l,LOW);
+  digitalWrite(p_back_l,HIGH);
+  digitalWrite(d_back_r,LOW);
+  digitalWrite(p_back_r,HIGH);
+}
+void right()
+{
+  digitalWrite(d_front_l,LOW);
+  digitalWrite(p_front_l,HIGH);
+  digitalWrite(d_front_r,LOW);
+  digitalWrite(p_front_r,HIGH);
+  digitalWrite(d_back_l,HIGH);
+  digitalWrite(p_back_l,HIGH);
+  digitalWrite(d_back_r,HIGH);
+  digitalWrite(p_back_r,HIGH);
+}
+void clockwise()
+{
+  digitalWrite(d_front_l,HIGH);
+  digitalWrite(p_front_l,HIGH);
+  digitalWrite(d_front_r,LOW);
+  digitalWrite(p_front_r,HIGH);
+  digitalWrite(d_back_l,HIGH);
+  digitalWrite(p_back_l,HIGH);
+  digitalWrite(d_back_r,LOW);
+  digitalWrite(p_back_r,HIGH);
+}
+void anti_clockwise()
+{
+  digitalWrite(d_front_l,LOW);
+  digitalWrite(p_front_l,HIGH);
+  digitalWrite(d_front_r,HIGH);
+  digitalWrite(p_front_r,HIGH);
+  digitalWrite(d_back_l,LOW);
+  digitalWrite(p_back_l,HIGH);
+  digitalWrite(d_back_r,HIGH);
+  digitalWrite(p_back_r,HIGH);
+}
+
+void s_stop()
+{
+  digitalWrite(p_front_l,LOW);
+  digitalWrite(p_front_r,LOW);
+  digitalWrite(p_back_l,LOW);
+  digitalWrite(p_back_r,LOW);
+  
+}
 void execution(char state){
   switch(state)
   {
     case 'W': {
   Serial.println("Forward");
-  //motor 1 - on
-  digitalWrite(motor1_IN1,HIGH);
-  digitalWrite(motor1_IN2,LOW);
-
-  //motor 2 - on
-  digitalWrite(motor2_IN3,HIGH);
-  digitalWrite(motor2_IN4,LOW);
-
-  //motor 3 - on
-  digitalWrite(motor3_IN5,HIGH);
-  digitalWrite(motor3_IN6,LOW);
-
-  //motor 4 - on
-  digitalWrite(motor4_IN7,HIGH);
-  digitalWrite(motor4_IN8,LOW);
+  forward();
   break;
 }
     case 'A': { 
   Serial.println("Moving Left");
-  
-  //motor 1 - off
-  digitalWrite(motor1_IN1,LOW);
-  digitalWrite(motor1_IN2,HIGH);
-
-  //motor 2 - on
-  digitalWrite(motor2_IN3,HIGH);
-  digitalWrite(motor2_IN4,LOW);
-
-  //motor 3 - on
-  digitalWrite(motor3_IN5,HIGH);
-  digitalWrite(motor3_IN6,LOW);
-
-  //motor 4 - off
-  digitalWrite(motor4_IN7,LOW);
-  digitalWrite(motor4_IN8,HIGH);
+  left();
   break;
     }
     case 'D': {
   Serial.println("Moving Right");
-
-  //motor 1 - on
-  digitalWrite(motor1_IN1,HIGH);
-  digitalWrite(motor1_IN2,LOW);
-
-  //motor 2 - off
-  digitalWrite(motor2_IN3,LOW);
-  digitalWrite(motor2_IN4,HIGH);
-
-  //motor 3 - off
-  digitalWrite(motor3_IN5,LOW);
-  digitalWrite(motor3_IN6,HIGH);
-
-  //motor 4 - on
-  digitalWrite(motor4_IN7,HIGH);
-  digitalWrite(motor4_IN8,LOW);
+  right();
   break;
 }
     case 'S': {
   Serial.println("Backward");
-
-  //motor 1 - off
-  digitalWrite(motor1_IN1,LOW);
-  digitalWrite(motor1_IN2,HIGH);
-
-  //motor 2 - off
-  digitalWrite(motor2_IN3,LOW);
-  digitalWrite(motor2_IN4,HIGH);
-
-  //motor 3 - off
-  digitalWrite(motor3_IN5,LOW);
-  digitalWrite(motor3_IN6,HIGH);
-
-  //motor 4 - off
-  digitalWrite(motor4_IN7,LOW);
-  digitalWrite(motor4_IN8,HIGH);
+  backward();
   break;
 }
     case 'R': {
   Serial.println("Rotating Clockwise/Right");
-
-  //motor 1 is on
-  digitalWrite(motor1_IN1,HIGH);
-  digitalWrite(motor1_IN2,LOW);
-
-  //motor 2 - off
-  digitalWrite(motor2_IN3,LOW);
-  digitalWrite(motor2_IN4,HIGH);
-
-  //motor 3 - on
-  digitalWrite(motor3_IN5,HIGH);
-  digitalWrite(motor3_IN6,LOW);
-
-  //motor 4 - off
-  digitalWrite(motor4_IN7,LOW);
-  digitalWrite(motor4_IN8,HIGH);
+  clockwise();
   break;
 }
-    case 'L': {
+    case 'L': {  
   Serial.println("Rotating AntiClockwise/Left");
-
-  //motor 1 - off
-  digitalWrite(motor1_IN1,LOW);
-  digitalWrite(motor1_IN2,HIGH);
-
-  //motor 2 - on
-  digitalWrite(motor2_IN3,HIGH);
-  digitalWrite(motor2_IN4,LOW);
-
-  //motor 3 - off
-  digitalWrite(motor3_IN5,LOW);
-  digitalWrite(motor3_IN6,HIGH);
-
-  //motor 4 - on
-  digitalWrite(motor4_IN7,HIGH);
-  digitalWrite(motor4_IN8,LOW);
+  anti_clockwise();
   break;
 }
+  case "-":
+  {
+    s_stop();
+  }
   }
 }
